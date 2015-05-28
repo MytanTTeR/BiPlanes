69c99ec2d31b832869e1b8d4c40bf422bbd812b0
HitechCraft <denis-borisov2@rambler.ru>
2015-05-28 21:32:55 +0300

    Creating scene borders

---
+++ Borders.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Borders : MonoBehaviour
+{
+    public Camera MainCamera;
+    int _screenWidth, _screenHeight;
+    Vector2 _objectPos;
+    Transform _transform;
+
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    void Update()
+    {
+        _screenWidth = Screen.width;
+        _screenHeight = Screen.height;
+
+        _objectPos = MainCamera.WorldToScreenPoint(transform.position);
+
+        CriticalPosition();
+    }
+
+    void CriticalPosition()
+    {
+        if (_objectPos.x >= _screenWidth)
+        {
+            _transform.position = MainCamera.ScreenToWorldPoint(new Vector3(0, _objectPos.y, 1));
+        }
+        else if (_objectPos.x <= 0)
+        {
+            _transform.position = MainCamera.ScreenToWorldPoint(new Vector3(_screenWidth, _objectPos.y, 1));
+        }
+    } 
+}
