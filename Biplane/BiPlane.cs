dd995f9d0d164d68695792911698aa2ad9120187
Тепляков Дмитрий Николаевич <dimtep95@gmail.com>
2015-05-29 21:45:33 +0300

    BiPlane Movement Fix

---
+++ Biplane/BiPlane.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BiPlaneController))]
+[RequireComponent(typeof(BiPlaneRigidbody))]
+[RequireComponent(typeof(Engine))]
+public class BiPlane : MonoBehaviour
+{
+    public float wingWidth, rotationScale;
+    Engine _engine;
+    BiPlaneRigidbody _rigidbody;
+    BiPlaneController _controller;
+    Transform _transform;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<BiPlaneRigidbody>();
+        _engine = GetComponent<Engine>();
+        _controller = GetComponent<BiPlaneController>();
+        _transform = GetComponent<Transform>();
+    } //+
+
+    void FixedUpdate()
+    {
+        UpdateForce();
+        UpdateRotation();
+        if (_engine.enabled)
+        {
+            _rigidbody.OtherForce = (_transform.right * _engine.Power).GetVector2();
+            _rigidbody.gravityScale = _rigidbody.mass - Mathf.Clamp(GetWing() * _engine.Power / 10f, 0f, _rigidbody.mass);
+        }
+        else
+        {
+            _rigidbody.gravityScale = 1;
+        }
+    } //Debug!!!
+
+    void UpdateForce()
+    {
+        if (_controller.AddForce) _engine.AddForce();
+        if (_controller.RemoveForce) _engine.RemoveForce();
+    } //+
+
+    void UpdateRotation()
+    {
+        Vector3 rot = Vector3.zero;
+        if (_controller.RotationUp) rot.z -= Time.deltaTime * rotationScale / wingWidth;
+        if (_controller.RotationDown) rot.z += Time.deltaTime * rotationScale / wingWidth;
+        _transform.localEulerAngles += rot;
+    } //?
+
+    public float GetWing()
+    {
+        return wingWidth - wingWidth * 90f / rotationAngle();
+    } //+
+
+    private float rotationAngle()
+    {
+        Vector2 direction = _transform.right.GetVector2();
+        float rotationLeft = Vector2.Angle(direction, -Vector2.right), rotationRight = Vector2.Angle(direction, Vector2.right);
+        if (rotationLeft > rotationRight) return rotationRight;
+        else return rotationLeft;
+    } //Test!!
+}
---
+++ Biplane/Controller/BiPlaneController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BiPlaneController : MonoBehaviour
+{
+    bool addForce, removeForce, rotationUp, rotationDown;
+
+    public bool RotationDown
+    {
+        get { return rotationDown; }
+    }
+
+    public bool RotationUp
+    {
+        get { return rotationUp; }
+    }
+
+    public bool RemoveForce
+    {
+        get { return removeForce; }
+    }
+
+    public bool AddForce
+    {
+        get { return addForce; }
+    }
+
+    void FixedUpdate()
+    {
+        UpdateParams();
+        if (Input.GetKey(KeyCode.D)) rotationUp = true;
+        if (Input.GetKey(KeyCode.A)) rotationDown = true;
+        if (Input.GetKey(KeyCode.W)) addForce = true;
+        if (Input.GetKey(KeyCode.S)) removeForce = true;
+    }
+
+    private void UpdateParams()
+    {
+        rotationUp = false;
+        rotationDown = false;
+        addForce = false;
+        removeForce = false;
+    }
+}
