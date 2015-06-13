bac216e148fee5acacd32ed42b2820e7a70762c2
Тепляков Дмитрий Николаевич <dimtep95@gmail.com>
2015-06-13 14:45:45 +0300

    Add Background Script

---
+++ UI/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    float screenWidth, screenHeight, backgroundWidth, backgroundHeight, realScreenWidth, realScreenHeight;
+    Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Texture2D backgroundTexture = GetComponent<SpriteRenderer>().sprite.texture;
+
+        backgroundHeight = backgroundTexture.height;
+        backgroundWidth = backgroundTexture.width;
+    }
+
+    void UpdateParams()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        realScreenHeight = 200f * mainCamera.orthographicSize;
+        realScreenWidth = screenWidth * realScreenHeight / screenHeight;
+    }
+
+    void Update()
+    {
+        UpdateParams();
+    }
+}
